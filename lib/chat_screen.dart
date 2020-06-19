4fe303460957f90edce6610658bf7c46ccde37ec
Sidi Becker <sidibecker@hotmail.com>
2020-06-19 00:00:52 -0300

    Creating chat screen

---
+++ lib/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:chat_firebase/text_composer.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Olá"),
+      ),
+      body: TextComposer(),
+    );
+  }
+}
